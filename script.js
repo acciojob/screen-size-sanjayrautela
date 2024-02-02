52caf635ae7e9b8583e337f45d8598414cc2d27f
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-02 15:50:34 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-//your JS code here. If required.
-function updateSizeInfo (){
-	const width = window.innerWidth;
-	const height = window.innerHeight;
-
-	document.getElementById('sizeInfo').innerHTML = `<h1>Width:${width} and Height:${height}</h1>`;
-}
-window.addEventListener('resize', updateSizeInfo);
-window.onload = updateSizeInfo;
